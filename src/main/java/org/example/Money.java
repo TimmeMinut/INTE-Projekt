0b8f2decfc79d22b03835a60f5049001f064130d
TimmeMinut <timorfalk@gmail.com>
2023-10-10 19:25:28 +0200

    Removed Currency class, and it's usages.

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,89 +1,47 @@
 package org.example;
 
-public class Money implements Comparable<Money> {
+public class Money {
 
-    private static final int MAJOR_UNIT = 100;
+    private long amount;
+    public static final int MAJOR_UNIT = 100;
 
-    private Currency currency;
-    private long ammount;
-
-    public Money(Currency currency, long ammount) {
-        if (currency == null)
-            throw new IllegalArgumentException("Currency can't be null");
-        if (ammount < 0)
+    public Money(long amount) {
+        if (amount < 0)
             throw new IllegalArgumentException(
-                    "Ammount can't be less than zero");
-        this.currency = currency;
-        this.ammount = ammount;
+                    "amount can't be less than zero");
+        this.amount = amount;
     }
 
-    public Money(Currency currency, long ammountOfMajorUnit,
-                 long ammountOfMinorUnit) {
-        this(currency, ammountOfMajorUnit * MAJOR_UNIT + ammountOfMinorUnit);
+    public Money(long amountOfMajorUnit, long amountOfMinorUnit) {
+        this(amountOfMajorUnit * MAJOR_UNIT + amountOfMinorUnit);
     }
 
-    public long getAmmountOfMajorUnit() {
-        return ammount / MAJOR_UNIT;
+    public long getAmountOfMajorUnit() {
+        return amount / MAJOR_UNIT;
     }
 
-    public long getAmmountOfMinorUnit() {
-        return ammount % MAJOR_UNIT;
+    public long getAmountOfMinorUnit() {
+        return amount % MAJOR_UNIT;
     }
 
-    public long getTotalAmmountInMinorUnit() {
-        return ammount;
+    public long getTotalAmountInMinorUnit() {
+        return amount;
     }
 
-    public Currency getCurrency() {
-        return currency;
+    public Money add(long amountOfMinorUnit) {
+        return new Money(amount + amountOfMinorUnit);
     }
 
-    public Money add(long ammountOfMinorUnit) {
-        return new Money(currency, ammount + ammountOfMinorUnit);
+
+    public Money subtract(long amountOfMinorUnit) {
+        return new Money(amount - amountOfMinorUnit);
     }
 
-    public Money add(Money addend) {
-        if (currency != addend.currency)
-            throw new IllegalArgumentException(
-                    "The currency needs to be the same in order to add");
-        return add(addend.ammount);
-    }
 
-    public Money subtract(long ammountOfMinorUnit) {
-        return new Money(currency, ammount - ammountOfMinorUnit);
-    }
-
-    public Money subtract(Money subtrahend) {
-        if (currency != subtrahend.currency)
-            throw new IllegalArgumentException(
-                    "The currency needs to be the same in order to subtract");
-        if (ammount < subtrahend.ammount)
-            throw new IllegalArgumentException(
-                    "You can't subtract more money than you have");
-        return subtract(subtrahend.ammount);
-    }
-
-    public int compareTo(Money other) {
-        if (currency != other.currency)
-            return currency.name.compareTo(other.currency.name);
-        return (int) (ammount - other.ammount);
-    }
-
-    public boolean equals(Object other) {
-        if (other instanceof Money) {
-            Money m = (Money) other;
-            return currency.equals(m.currency) && ammount == m.ammount;
-        } else {
-            return false;
-        }
-    }
-
+    @Override
     public String toString() {
-        return String.format("%d:%d %s", getAmmountOfMajorUnit(),
-                getAmmountOfMinorUnit(), getCurrency().symbol);
+        return String.format("%d.%d", getAmountOfMajorUnit(), getAmountOfMinorUnit());
     }
 
-    public Money getNothing() {
-        return new Money(currency, 0L);
-    }
+
 }
