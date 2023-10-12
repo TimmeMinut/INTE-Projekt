d6abdfb12aee99a4f3208adc9ac694be94c9bc63
TimmeMinut <timorfalk@gmail.com>
2023-10-12 10:37:21 +0200

    Add function to increase Bonus points.

--- src/main/java/org/example/Membership.java
+++ src/main/java/org/example/Membership.java
@@ -9,10 +9,14 @@ public class Membership {
     // Se Henriks video om mock-objekt / test-doubles
     private LocalDateTime startingDateTime;
     private Customer customer;
+    private int points;
+    private String level; // Bronze, Silver, Gold
 
     public Membership(Customer customer) {
         this.customer = customer;
-        startingDateTime = LocalDateTime.now();
+        this.startingDateTime = LocalDateTime.now();
+        this.points = 0;
+        this.level = "Bronze";
     }
 
     public LocalDateTime getStartingDateTime() {
@@ -23,8 +27,34 @@ public class Membership {
         return formatter.format(startingDateTime);
     }
 
-
     public Customer getCustomer() {
         return customer;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void increasePoints(long payment) {
+        // Ska öka baserat på hur mkt kund betalar vid ett köp, och deras nivå.
+        // Konvertering från long till int, vad händer vid enormt köp?
+        double bonusFactor;
+        switch (this.getLevel()) {
+            case "Bronze" -> bonusFactor = 0.25;
+            case "Silver" -> bonusFactor = 0.50;
+            case "Gold" -> bonusFactor = 0.75;
+            default -> bonusFactor = 0;
+        }
+
+        double rawPoints = (double) (payment * bonusFactor) * 0.1 * 0.1;
+        this.points += (int) Math.round(rawPoints);
+    }
+
+    public void changeLevel() {
+
     }
 }
--- src/test/java/org/example/MembershipTest.java
+++ src/test/java/org/example/MembershipTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -7,13 +8,19 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MembershipTest {
-    public static final Customer VALID_CUSTOMER = new Customer("Bob", "20001231-1234", 15000_00, 500_00);
+    public static final Customer NON_MEMBER_CUSTOMER = new Customer("Bob", "20001231-1234", 15000_00, 500_00);
+    public static final Customer MEMBER_CUSTOMER = new Customer("Miriam", "19990115-2345", 15000_00, 500_00);
     public static final LocalDateTime VALID_DATE_TIME = LocalDateTime.of(2023, 1, 31, 15, 30);
+
+    @BeforeAll
+    public static void initMemberCustomer() {
+        MEMBER_CUSTOMER.becomeMember();
+    }
 
     @Test
     void Membership_is_created_for_correct_customer() {
         // given
-        Customer customer = VALID_CUSTOMER;
+        Customer customer = NON_MEMBER_CUSTOMER;
 
         // when
         customer.becomeMember();
@@ -26,7 +33,7 @@ public class MembershipTest {
     @Test
     void Membership_starting_date_is_recorded() {
         // Given
-        Customer customer = VALID_CUSTOMER;
+        Customer customer = NON_MEMBER_CUSTOMER;
 
         // When
         customer.becomeMember();
@@ -38,7 +45,7 @@ public class MembershipTest {
     @Test
     void Membership_starting_date_is_formatted_correctly() {
         // Given
-        Customer customer = VALID_CUSTOMER;
+        Customer customer = NON_MEMBER_CUSTOMER;
         customer.becomeMember();
         Membership membership = customer.getMembership();
         String correctDateTimeFormat = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}";
@@ -49,5 +56,19 @@ public class MembershipTest {
 
         // Then
         assertTrue(formattedDateTime.matches(correctDateTimeFormat));
+    }
+
+    @Test
+    void Bonus_points_increase_after_purchase() {
+        // Given
+        Customer customer = MEMBER_CUSTOMER;
+        Membership membership = customer.getMembership();
+
+        // When
+        customer.checksOut();
+        membership.increasePoints(1000_00);
+
+        // Then
+        assertEquals(250, membership.getPoints());
     }
 }
